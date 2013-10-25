fa41c485192a4a979512a710b438c98547624d35
Jan Hula <jan.hula21@gmail.com>
2013-10-25 15:34:49 +0200

    second commmit

---
+++ tests/basic/mySpec.js
@@ -0,0 +1,10 @@
+/**
+ * Created by honza on 10/23/13.
+ */
+describe("my suite",function() {
+    it("should be true",function(){
+        expect(true).toBe(ture);
+    })
+})
+
+//TODO[jan] create new tests and even more
